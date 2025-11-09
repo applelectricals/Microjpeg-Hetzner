06f8659d4d8a091ae38464e9a626c8a3edabaec3
Prasun <applelectricals@gmail.com>
2025-11-09 17:56:13 +0530

    fix: remove duplicate CheckoutPage import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import DynamicCompressPage from './pages/DynamicCompressPage';
-import CheckoutPage from './pages/CheckoutPage';  // ← ADD THIS
 import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
