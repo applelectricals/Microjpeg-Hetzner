33db2f2c82b9f3e7bdd820f9981624aa530c40cf
Prasun <applelectricals@gmail.com>
2025-11-14 12:00:43 +0530

    Applied Dark Theme to API pages 3

--- client/src/pages/api-docs.tsx
+++ client/src/pages/api-docs.tsx
@@ -157,7 +157,7 @@ activeTab === id
         </div>
       </div>
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 relative z-10">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 relative z-10 min-h-screen">
         {/* Overview Tab */}
         {activeTab === "overview" && (
           <div className="space-y-8">
